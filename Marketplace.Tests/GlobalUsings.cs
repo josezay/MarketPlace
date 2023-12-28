af164e264f13f77e64874d79626e38714b3f5d36
José Zay <josezay22@gmail.com>
2023-12-28 19:03:33 -0300

    escrever teste para classe Money.

--- Marketplace.Tests/GlobalUsings.cs
+++ Marketplace.Tests/GlobalUsings.cs
@@ -1,1 +1,5 @@
 global using Xunit;
+using Marketplace.Tests;
+
+var moneyTest = new MoneyTest();
+moneyTest.Money_objects_with_the_same_amount_should_be_equal();
---
+++ Marketplace.Tests/MoneyTest.cs
@@ -0,0 +1,21 @@
+﻿using Marketplace.Domain;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Marketplace.Tests
+{
+    public class MoneyTest
+    {
+        [Fact]
+        public void Money_objects_with_the_same_amount_should_be_equal()
+        {
+            var firstAmount = new Money(5);
+            var secondAmount = new Money(5);
+
+            Assert.NotEqual(firstAmount, secondAmount);
+        }
+    }
+}
