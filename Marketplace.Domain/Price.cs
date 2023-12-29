65408c7cb9c092e8815eb4aa890eb635ddde3371
José Zay <josezay22@gmail.com>
2023-12-29 17:35:42 -0300

    adicionar modelo de domínio Price.

---
+++ Marketplace.Domain/Price.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Marketplace.Domain
+{
+    public class Price : Money
+    {
+        public Price(decimal amount) : base(amount)
+        {
+            if (amount < 0) 
+            {  
+                throw new ArgumentException("Preço não pode ser negativo");
+            }
+        }
+    }
+}
