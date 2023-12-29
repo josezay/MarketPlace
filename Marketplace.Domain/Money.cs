16ed33d0c8ec11626da8ba789e2eb4aa8fa1abfc
José Zay <josezay22@gmail.com>
2023-12-29 16:38:34 -0300

    simplificar classe money.

--- Marketplace.Domain/Money.cs
+++ Marketplace.Domain/Money.cs
@@ -3,38 +3,16 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Marketplace.Framework;
 
 namespace Marketplace.Domain
 {
-    public class Money
+    public class Money : Value<Money>
     {
         public decimal Amount { get; }
         public Money(decimal amount)
         {
             Amount = amount;
         }
-
-        public bool Equals(Money? other)
-        {
-            if (ReferenceEquals(null, other)) return false;
-            if (ReferenceEquals(this, other)) return true;
-            return Amount.Equals(other.Amount);
-        }
-
-        public override bool Equals(object? obj)
-        {
-            if (ReferenceEquals (null, obj)) return false;
-            if (ReferenceEquals (this, obj)) return true;
-            if (obj.GetType () != typeof (Money)) return false;
-            return Equals((Money?) obj);
-        }
-
-        public override int GetHashCode()
-        {
-            return Amount.GetHashCode();
-        }
-
-        public static bool operator ==(Money? left, Money? right) => Equals(left, right);
-        public static bool operator !=(Money? left, Money? right) => !Equals(left, right);
     }
 }
