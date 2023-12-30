1e3c5b594e751eb2d340df57541307b4a857ba91
José Zay <josezay22@gmail.com>
2023-12-30 13:58:26 -0300

    Criar um Factory de ClassifiedAdTitle.

---
+++ Marketplace.Domain/ClassifiedAdTitle.cs
@@ -0,0 +1,23 @@
+﻿using Marketplace.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Marketplace.Domain
+{
+    public class ClassifiedAdTitle : Value<ClassifiedAdTitle>
+    {
+        public static ClassifiedAdTitle FromString(string title) => new ClassifiedAdTitle(title);
+        private readonly string _value;
+        private ClassifiedAdTitle(string value)
+        {
+            if (value.Length > 100)
+            {
+                throw new ArgumentOutOfRangeException("Título não pode ter mais de 100 caracteres", nameof(value);
+            }
+            _value = value;
+        }
+    }
+}
