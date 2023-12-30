e8211c97267750fa03ba145a3513341165110dfc
José Zay <josezay22@gmail.com>
2023-12-30 09:37:24 -0300

    implementar soma e subtração monetária em Money.

--- Marketplace.Domain/Money.cs
+++ Marketplace.Domain/Money.cs
@@ -14,5 +14,11 @@ namespace Marketplace.Domain
         {
             Amount = amount;
         }
+        
+        public Money Add(Money summand) => new Money(Amount + summand.Amount);
+        public Money Subtract(Money subtrahend) => new Money(Amount - subtrahend.Amount);
+
+        public static Money operator +(Money summand1, Money summand2) => summand1.Add(summand2);
+        public static Money operator -(Money minuend, Money subtrahend) => minuend.Subtract(subtrahend);
     }
 }
