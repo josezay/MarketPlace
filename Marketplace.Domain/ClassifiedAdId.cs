83f89a6c7af0c64cb738b02d2d182e40f2fc1ad1
José Zay <josezay22@gmail.com>
2023-12-30 13:46:40 -0300

    Tornar classes UserId e ClassifiedAdId de objetos de valor.

--- Marketplace.Domain/ClassifiedAdId.cs
+++ Marketplace.Domain/ClassifiedAdId.cs
@@ -3,10 +3,11 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Marketplace.Framework;
 
 namespace Marketplace.Domain
 {
-    public class ClassifiedAdId
+    public class ClassifiedAdId : Value<ClassifiedAdId>
     {
         private readonly Guid _value;
 
--- Marketplace.Domain/UserId.cs
+++ Marketplace.Domain/UserId.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Marketplace.Framework;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +7,7 @@ using System.Threading.Tasks;
 
 namespace Marketplace.Domain
 {
-    public class UserId
+    public class UserId : Value<UserId>
     {
         private readonly Guid _value;
 
